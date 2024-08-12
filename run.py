2432bc309fd265a870082ccc2bff7cc4ba2832e9
mizjaggy18 <77017051+mizjaggy18@users.noreply.github.com>
2024-08-12 21:24:31 +0800

    Update run.py

--- run.py
+++ run.py
@@ -77,7 +77,7 @@ def run(cyto_job, parameters):
 
     # Paths where ONNX and OpenVINO IR models will be stored.
     # ir_path = weights_path.with_suffix(".xml")
-    ir_path = "/models/pdac-3class_dn21adam_best_model_100ep.xml"
+    ir_path = "/models/pdac-validated_dn21adam_best_model_100ep.xml"
 
     # Instantiate OpenVINO Core
     core = ov.Core()
